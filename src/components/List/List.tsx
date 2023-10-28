301867d11f2ed3243e90ad7b0c03ec212d95c578
Mikołaj Wiśniewski <raev@MacBook-Pro-Mikoaj.local>
2023-10-28 13:14:11 +0200

    fix: fix condition

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -11,7 +11,7 @@ export const List = () => {
 			<div className="w-11/12 py-4 max-w-5xl max-h-96 bg-sky-500 rounded-md grid place-items-center font-bold text-lg text-white gap-4 overflow-y-auto">
 				{isLoading ? (
 					<Spinner />
-				) : isSuccess && data.animals ? (
+				) : isSuccess && data.animals.legnth > 0 ? (
 					data.animals.map((animal: { id: number; name: string; weight: number }) => (
 						<ListItem key={animal.id} id={animal.id} name={animal.name} weight={animal.weight} />
 					))
